56a42523de5b443ba22ac5403d3e92f49191885e
Simona Lorenc <lorenc.simona@gmail.com>
2023-10-13 15:39:19 +0200

    display exchange rates in detail

--- src/app/currency.ts
+++ src/app/currency.ts
@@ -12,3 +12,16 @@ export interface Rate {
   code: string
   mid: number
 }
+//exchange rates from last 7 days
+export interface Root {
+  table: string
+  currency: string
+  code: string
+  rates: DetailRate[]
+}
+
+export interface DetailRate {
+  no: string
+  effectiveDate: string
+  mid: number
+}
