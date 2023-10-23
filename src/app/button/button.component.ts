ed12d4e8d6d615d211956f92f260cb242bfaaecf
Simona Lorenc <lorenc.simona@gmail.com>
2023-10-23 20:05:27 +0200

    add button active

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,11 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent {
-  @Input() buttonText!: string
+export class ButtonComponent implements OnInit{
   @Input() size!: string
+  @Input() buttonText!: string
+  @Input() isActive: boolean = false
+  @Output() onClickBtn = new EventEmitter();
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    this.onClickBtn.emit();
+  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  isCurrenciesActive: boolean = true
+  isGoldActive: boolean = false
+
+  ngOnInit(): void {
+  
+  }
+
+  onClickCurrencies() {
+    this.isCurrenciesActive = true
+    this.isGoldActive = false
+  }
+
+  onClickGold() {
+    this.isCurrenciesActive = false
+    this.isGoldActive = true
+  }
+
+  
 }
