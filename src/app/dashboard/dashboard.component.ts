a027e47b1cf225f9c18ceea12de0a99dfd499570
Simona Lorenc <lorenc.simona@gmail.com>
2023-10-23 21:18:50 +0200

    change routing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent {
   isCurrenciesActive: boolean = true
 
+  constructor(private router: Router) {}
+
   onClickCurrencies() {
     this.isCurrenciesActive = true
+    this.router.navigate(['/dashboard/app-currency-list'])
+    
   }
 
   onClickGold() {
     this.isCurrenciesActive = false
+    this.router.navigate(['/dashboard/gold-prices'])
   }
 }
