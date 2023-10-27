72c3f0bd6566f793114d01ad2582606f3a3289a5
Simona Lorenc <lorenc.simona@gmail.com>
2023-10-28 00:30:22 +0200

    appearance ui

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, HostListener, Renderer2, AfterViewInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,34 +7,37 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('transparentBackground', [
+      state('true', style({backgroundColor: 'transparent'})),
+      state('false', style({backgroundColor: '#FFC400'})), //TODO: wykorzystac kolor bazowy
+      transition( '* <=> *', animate(300))
+    ])
+  ]
 })
 export class NavbarComponent {
+  private TRANSPARENT_SCROLL_OFFSET: number = 40
+  isTransparent: boolean = true
   isCollapsed = true;
   toggleIcon = faBars;
-  isBackgroundWhite: boolean = false
-  isNavbarScrolled: boolean = false
-
-  constructor(private elementRef: ElementRef) {}
 
   toggleCollapse() {
-    const modalDiv = this.elementRef.nativeElement.querySelector('#myModal')
-    const collapsedMenu = this.elementRef.nativeElement.querySelector('.collapse')
-    this.isCollapsed = !this.isCollapsed;
-    this.isBackgroundWhite = !this.isBackgroundWhite
+    this.isCollapsed = !this.isCollapsed
     if(this.isCollapsed) {
       this.toggleIcon = faBars
-      modalDiv!.style.display = 'none'
-      collapsedMenu.style.paddingBottom = '0'
     } else {
       this.toggleIcon = faXmark
-      modalDiv!.style.display = 'block'
-      collapsedMenu.style.paddingBottom = '20px'
     }
+    this.isTransparent = this.isCollapsed && this.isTransparentScrollOffset()
   }
 
   @HostListener('window: scroll', ['$event'])
-  addBackgroundToNavbar(event: Event) {
-    this.isBackgroundWhite = window.scrollY >= 56;
-    this.isNavbarScrolled = window.scrollY >= 56;
+  private onScroll(event: Event): void {
+    if(!this.isCollapsed) return
+    this.isTransparent = this.isTransparentScrollOffset()
+  }
+
+  private isTransparentScrollOffset() {
+    return window.scrollY < this.TRANSPARENT_SCROLL_OFFSET
   }
 }
